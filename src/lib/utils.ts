8ca4e1c73c01097203262eb3c4445da84d148056
Kripa Sindhu <sindhukripa007@gmail.com>
2026-03-20 18:25:33 +0530

    fixing typescript checks

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-
-/**
- * Merge Tailwind classes safely. Install clsx if using this utility:
- * npm install clsx
- */
-export function cn(...inputs: ClassValue[]) {
-  return clsx(inputs);
-}
