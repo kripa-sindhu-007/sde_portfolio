be588ea794fa7f35e0bd3054711988542aa97dfb
Kripa Sindhu <sindhukripa007@gmail.com>
2026-04-17 16:50:14 +0530

    added site metrics tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import {
   Syne,
 } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const outfit = Outfit({
@@ -101,6 +102,7 @@ export default function RootLayout({
       </head>
       <body className="min-h-screen antialiased cursor-default">
         {children}
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
